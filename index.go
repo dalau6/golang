0ccfb9b57281c2e875f53fc55d24caf271439b1c
David <dalau6@gmail.com>
2020-02-29 22:45:10 -0800

    nested loop.

--- index.go
+++ index.go
@@ -1,22 +1,20 @@
+// The following program uses a nested for loop to find the prime numbers from 2 to 100 −
 package main
 
 import "fmt"
 
 func main() {
-	var b int = 15
-	var a int
-	numbers := [6]int{1, 2, 3, 5} 
- 
-	/* for loop execution */
-	for a := 0; a < 10; a++ {
-	   fmt.Printf("value of a: %d\n", a)
+	/* local variable definition */
+	var i, j int
+
+	for i = 2; i < 100; i++ {
+		for j = 2; j <= (i / j); j++ {
+			if i%j == 0 {
+				break // if factor found, not prime
+			}
+		}
+		if j > (i / j) {
+			fmt.Printf("%d is prime\n", i)
+		}
 	}
-	for a < b {
-	   a++
-	   fmt.Printf("value of a: %d\n", a)
-	}
-	for i,x:= range numbers {
-	   fmt.Printf("value of x = %d at %d\n", x,i)
-	}   
- }
- 
+}
