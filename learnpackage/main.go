a30365d2bd0ffadfe0c1efccee9f1c6196cb5127
David <dalau6@gmail.com>
2020-03-21 21:27:31 -0700

    Refactored calc ch to use range loop.

--- learnpackage/main.go
+++ learnpackage/main.go
@@ -39,12 +39,21 @@ func alphabets() {
 	}
 }
 
-func calcSquares(number int, squareop chan<- int) {
-	sum := 0
+func digits(number int, dchnl chan<- int) {
 	for number != 0 {
 		digit := number % 10
-		sum += digit * digit
+		dchnl <- digit
 		number /= 10
+	}
+	close(dchnl)
+}
+
+func calcSquares(number int, squareop chan<- int) {
+	sum := 0
+	dch := make(chan int)
+	go digits(number, dch)
+	for digit := range dch {
+		sum += digit * digit
 	}
 	squareop <- sum
 	close(squareop)
@@ -52,13 +61,20 @@ func calcSquares(number int, squareop chan<- int) {
 
 func calcCubes(number int, cubeop chan<- int) {
 	sum := 0
-	for number != 0 {
-		digit := number % 10
+	dch := make(chan int)
+	go digits(number, dch)
+	for digit := range dch {
 		sum += digit * digit * digit
-		number /= 10
 	}
 	cubeop <- sum
 	close(cubeop)
+}
+
+func producer(chnl chan<- int) {
+	for i := 0; i < 10; i++ {
+		chnl <- i
+	}
+	close(chnl)
 }
 
 func main() {
@@ -80,11 +96,17 @@ func main() {
 		squares, ok := <-sqrch
 		cubes, okay := <-cubech
 		if ok == false && okay == false {
-			// squares, cubes := <-sqrch, <-cubech
-			fmt.Println("Final output", squares+cubes)
 			break
 		}
 		fmt.Println("Received ", squares, cubes)
+		// squares, cubes := <-sqrch, <-cubech
+		fmt.Println("Final output", squares+cubes)
+	}
+
+	ch := make(chan int)
+	go producer(ch)
+	for v := range ch {
+		fmt.Println("Received ", v)
 	}
 
 	go numbers()
