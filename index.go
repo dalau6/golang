88eec23a0f25d3b4baea6732de19b13b3a5561ae
David <dalau6@gmail.com>
2020-03-23 00:00:15 -0700

    Used Go Workerpool.

--- index.go
+++ index.go
@@ -2,24 +2,74 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
-func numbers() {
-	for i := 1; i <= 5; i++ {
-		time.Sleep(250 * time.Millisecond)
-		fmt.Printf("%d ", i)
-	}
+type Job struct {
+	id       int
+	randomno int
 }
-func alphabets() {
-	for i := 'a'; i <= 'e'; i++ {
-		time.Sleep(400 * time.Millisecond)
-		fmt.Printf("%c ", i)
+type Result struct {
+	job         Job
+	sumofdigits int
+}
+
+var jobs = make(chan Job, 10)
+var results = make(chan Result, 10)
+
+func digits(number int) int {
+	sum := 0
+	no := number
+	for no != 0 {
+		digit := no % 10
+		sum += digit
+		no /= 10
 	}
+	time.Sleep(2 * time.Second)
+	return sum
+}
+func worker(wg *sync.WaitGroup) {
+	for job := range jobs {
+		output := Result{job, digits(job.randomno)}
+		results <- output
+	}
+	wg.Done()
+}
+func createWorkerPool(noOfWorkers int) {
+	var wg sync.WaitGroup
+	for i := 0; i < noOfWorkers; i++ {
+		wg.Add(1)
+		go worker(&wg)
+	}
+	wg.Wait()
+	close(results)
+}
+func allocate(noOfJobs int) {
+	for i := 0; i < noOfJobs; i++ {
+		randomno := rand.Intn(999)
+		job := Job{i, randomno}
+		jobs <- job
+	}
+	close(jobs)
+}
+func result(done chan bool) {
+	for result := range results {
+		fmt.Printf("Job id %d, input random no %d , sum of digits %d\n", result.job.id, result.job.randomno, result.sumofdigits)
+	}
+	done <- true
 }
 func main() {
-	go numbers()
-	go alphabets()
-	time.Sleep(3000 * time.Millisecond)
-	fmt.Println("main terminated")
+	startTime := time.Now()
+	noOfJobs := 100
+	go allocate(noOfJobs)
+	done := make(chan bool)
+	go result(done)
+	noOfWorkers := 20
+	createWorkerPool(noOfWorkers)
+	<-done
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	fmt.Println("total time taken ", diff.Seconds(), "seconds")
 }
