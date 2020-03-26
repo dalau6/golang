5a439dd9c7bc304ea538be56ae35d0b7f9304e36
David <dalau6@gmail.com>
2020-03-26 00:35:52 -0700

    defered.

--- oop/main.go
+++ oop/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"oop/employee"
+	"sync"
 )
 
 type author struct {
@@ -136,4 +137,39 @@ func main() {
 	popupAd := Advertisement{adName: "Popup Ad", CPC: 5, noOfClicks: 750}
 	incomeStreams := []Income{project1, project2, project3, bannerAd, popupAd}
 	calculateNetIncome(incomeStreams)
+
+	var wg sync.WaitGroup
+
+	r1 := rect{-67, 89}
+	r2 := rect{5, -67}
+	r3 := rect{8, 9}
+	rects := []rect{r1, r2, r3}
+
+	for _, v := range rects {
+		wg.Add(1)
+		go v.area(&wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("All go routines finished executing")
+}
+
+type rect struct {
+	length int
+	width  int
+}
+
+func (r rect) area(wg *sync.WaitGroup) {
+	defer wg.Done()
+	if r.length < 0 {
+		fmt.Printf("rect %v's length should be greater than zero\n", r)
+		return
+	}
+	if r.width < 0 {
+		fmt.Printf("rect %v's width should be greater than zero\n", r)
+		return
+	}
+	area := r.length * r.width
+	fmt.Printf("rect %v's area %d\n", r, area)
 }
