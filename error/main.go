ae1fbb8d5710553b377ca71882ab9fbfaffef545
David <dalau6@gmail.com>
2020-03-27 00:59:24 -0700

    Refactored errors.

--- error/main.go
+++ error/main.go
@@ -7,38 +7,41 @@ import (
 	"path/filepath"
 )
 
-/* It contains a single method with signature Error() string.
-Any type which implements this interface can be used as an error.
-This method provides the description of the error.
+/*
+	It contains a single method with signature Error() string.
+	Any type which implements this interface can be used as an error.
+	This method provides the description of the error.
+
+	type error interface {
+		Error() string
+	}
+
+	func (e *PathError) Error() string { return e.Op + " " + e.Path + ": " + e.Err.Error() }
 */
-// type error interface {
-//     Error() string
-// }
-
-/* If you read the documentation of the Open function carefully,
-you can see that it returns an error of type *PathError.
-PathError is a struct type and its implementation in the standard library is as follows,
-*/
-// type PathError struct {
-// 	Op   string
-// 	Path string
-// 	Err  error
-// }
-
-// func (e *PathError) Error() string { return e.Op + " " + e.Path + ": " + e.Err.Error() }
-
-// ErrBadPattern is defined in the filepath package as follows.
-// var ErrBadPattern = errors.New("syntax error in pattern")
-
-func main() {
+func open() bool {
 	f, err := os.Open("/test.txt")
 	if err, ok := err.(*os.PathError); ok {
 		fmt.Println("File at path", err.Path, "failed to open")
-		return
+		return true
 	}
 	fmt.Println(f.Name(), "opened successfully")
+	return false
+}
 
-	// DNS lookups
+/*
+	DNS lookups
+
+	If you read the documentation of the Open function carefully,
+	you can see that it returns an error of type *PathError.
+	PathError is a struct type and its implementation in the standard library is as follows,
+
+	type PathError struct {
+		Op   string
+		Path string
+		Err  error
+	}
+*/
+func lookup() {
 	addr, err := net.LookupHost("golangbot123.com")
 	if err, ok := err.(*net.DNSError); ok {
 		if err.Timeout() {
@@ -51,12 +54,24 @@ func main() {
 		return
 	}
 	fmt.Println(addr)
+}
 
-	// This function returns an error ErrBadPattern when the pattern is malformed.
+/*
+	ErrBadPattern is defined in the filepath package as follows.
+	var ErrBadPattern = errors.New("syntax error in pattern")
+	This function returns an error ErrBadPattern when the pattern is malformed.
+*/
+func badpattern() {
 	files, error := filepath.Glob("[")
 	if error != nil && error == filepath.ErrBadPattern {
 		fmt.Println(error)
 		return
 	}
 	fmt.Println("matched files", files)
+}
+
+func main() {
+	open()
+	lookup()
+	badpattern()
 }
