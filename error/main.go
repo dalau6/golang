ad9419f6609607746684b4e35b499c3b0b213dae
David <dalau6@gmail.com>
2020-03-28 00:38:22 -0700

    Provided more info using structs and interface.

--- error/main.go
+++ error/main.go
@@ -92,9 +92,19 @@ func badpattern() {
 
 func circleArea(radius float64) (float64, error) {
 	if radius < 0 {
-		return 0, fmt.Errorf("Area calculation failed, radius %0.2f is less than zero", radius)
+		// return 0, fmt.Errorf("Area calculation failed, radius %0.2f is less than zero", radius)
+		return 0, &areaError{"radius is negative", radius}
 	}
 	return math.Pi * radius * radius, nil
+}
+
+type areaError struct {
+	err    string
+	radius float64
+}
+
+func (e *areaError) Error() string {
+	return fmt.Sprintf("radius %0.2f: %s", e.radius, e.err)
 }
 
 func main() {
@@ -106,6 +116,10 @@ func main() {
 	radius := -20.0
 	area, err := circleArea(radius)
 	if err != nil {
+		if err, ok := err.(*areaError); ok {
+			fmt.Printf("Radius %0.2f is less than zero", err.radius)
+			return
+		}
 		fmt.Println(err)
 		return
 	}
