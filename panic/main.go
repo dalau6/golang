1b0e111e7f4de559b957be88cd51a1526aa33818
David <dalau6@gmail.com>
2020-03-28 19:03:08 -0700

    Panics w/ Recover.

--- panic/main.go
+++ panic/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"runtime/debug"
 )
 
 // The signature of the built in panic function
 // func panic(interface{})
 
+// The signature of recover function
+// func recover() interface{}
+
+func recoverName() {
+	if r := recover(); r != nil {
+		fmt.Println("recovered from ", r)
+		debug.PrintStack()
+	}
+}
+
 func fullName(firstName *string, lastName *string) {
-	defer fmt.Println("deferred call in fullName")
+	defer recoverName()
 	if firstName == nil {
 		panic("runtime error: first name cannot be nil")
 	}
@@ -19,9 +30,19 @@ func fullName(firstName *string, lastName *string) {
 	fmt.Println("returned normally from fullName")
 }
 
+func a() {
+	defer recoverName()
+	n := []int{5, 7, 4}
+	fmt.Println(n[3])
+	fmt.Println("normally returned from a")
+}
+
 func main() {
 	defer fmt.Println("deferred call in main")
 	firstName := "Elon"
 	fullName(&firstName, nil)
 	fmt.Println("returned normally from main")
+
+	a()
+	fmt.Println("normally returned from main")
 }
