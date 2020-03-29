916483a5d8fbbb3ed8ddab8e588d2ede88fe8997
David <dalau6@gmail.com>
2020-03-29 13:21:07 -0700

    Reading a file in small chunks.

--- files/main.go
+++ files/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
-
-	"github.com/gobuffalo/packr"
+	"log"
+	"os"
+	// "github.com/gobuffalo/packr"
 )
 
 func main() {
-	box := packr.NewBox("./filehandling") // A box represents a folder whose contents will be embedded in the binary.
-	data := box.String("test.txt")        // read the contents of the file.
-	fmt.Println("Contents of file:", data)
+	// box := packr.NewBox("./filehandling") // A box represents a folder whose contents will be embedded in the binary.
+	// data := box.String("test.txt")        // read the contents of the file.
+	// fmt.Println("Contents of file:", data)
+
+	fptr := flag.String("fpath", "test.txt", "file path to read from")
+	flag.Parse()
+
+	f, err := os.Open(*fptr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	r := bufio.NewReader(f)
+	b := make([]byte, 3)
+	for {
+		n, err := r.Read(b)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			break
+		}
+		fmt.Println(string(b[0:n]))
+	}
 }
