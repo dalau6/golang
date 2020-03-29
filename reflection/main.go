47def21e17da435f374bc001ab2343f351349e44
David <dalau6@gmail.com>
2020-03-29 00:24:03 -0700

    Complete Reflection Program.

--- reflection/main.go
+++ reflection/main.go
@@ -10,6 +10,14 @@ type order struct {
 	customerId int
 }
 
+type employee struct {
+	name    string
+	id      int
+	address string
+	salary  int
+	country string
+}
+
 func createQuery(q interface{}) {
 	t := reflect.TypeOf(q)
 	k := t.Kind()
@@ -17,12 +25,34 @@ func createQuery(q interface{}) {
 	fmt.Println("Kind ", k)
 
 	if reflect.ValueOf(q).Kind() == reflect.Struct {
+		t := reflect.TypeOf(q).Name()
+		query := fmt.Sprintf("insert into %s values(", t)
 		v := reflect.ValueOf(q)
-		fmt.Println("Number of fields", v.NumField())
 		for i := 0; i < v.NumField(); i++ {
-			fmt.Printf("Field:%d type:%T value:%v\n", i, v.Field(i), v.Field(i))
+			switch v.Field(i).Kind() {
+			case reflect.Int:
+				if i == 0 {
+					query = fmt.Sprintf("%s%d", query, v.Field(i).Int())
+				} else {
+					query = fmt.Sprintf("%s, %d", query, v.Field(i).Int())
+				}
+			case reflect.String:
+				if i == 0 {
+					query = fmt.Sprintf("%s\"%s\"", query, v.Field(i).String())
+				} else {
+					query = fmt.Sprintf("%s, \"%s\"", query, v.Field(i).String())
+				}
+			default:
+				fmt.Println("Unsupported type")
+				return
+			}
 		}
+		query = fmt.Sprintf("%s)", query)
+		fmt.Println(query)
+		return
+
 	}
+	fmt.Println("unsupported type")
 
 }
 
@@ -33,4 +63,14 @@ func main() {
 	}
 	createQuery(o)
 
+	e := employee{
+		name:    "Naveen",
+		id:      565,
+		address: "Coimbatore",
+		salary:  90000,
+		country: "India",
+	}
+	createQuery(e)
+	i := 90
+	createQuery(i)
 }
