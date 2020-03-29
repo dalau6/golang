87275ff17f253110996905e83b90e556dbac1ae5
David <dalau6@gmail.com>
2020-03-29 00:00:49 -0700

    NumField only on Structs.

--- reflection/main.go
+++ reflection/main.go
@@ -12,11 +12,20 @@ type order struct {
 
 func createQuery(q interface{}) {
 	t := reflect.TypeOf(q)
-	v := reflect.ValueOf(q)
+	k := t.Kind()
 	fmt.Println("Type ", t)
-	fmt.Println("Value ", v)
+	fmt.Println("Kind ", k)
+
+	if reflect.ValueOf(q).Kind() == reflect.Struct {
+		v := reflect.ValueOf(q)
+		fmt.Println("Number of fields", v.NumField())
+		for i := 0; i < v.NumField(); i++ {
+			fmt.Printf("Field:%d type:%T value:%v\n", i, v.Field(i), v.Field(i))
+		}
+	}
 
 }
+
 func main() {
 	o := order{
 		ordId:      456,
